7f94de198af3b407d8795d435dcb0f98088e4191
Yash Kumar <yash131105@gmail.com>
2026-08-11 18:09:48 +0530

    Day 3: If-else, nested conditions, ASCII comparisons, typecasting, and revision practice completed

--- DSA_Practice/Day03_Loops_and_Patternprinting/q09_reverse.cpp
+++ DSA_Practice/Day03_Loops_and_Patternprinting/q09_reverse.cpp
@@ -1,23 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<<"Enter the input: ";
-    cin>>n;
-    int r = 0;
-   
-    while(n!=0){
-        int ld = n%10;
-        r += ld;
-        r *= 10;
-        n /= 10;}
-        cout<<r;
+#include<cmath>
+
+
+int main()
+{
+    int n = 1234;
+    int u = n;
+    int j = 0;
+    int ld;
+    int r;
+    int sum = 0;
+    while(n > 0){
+        n /= 10;
+        j++;
+    }
+    for(int i=1; i<=j ; i++){
+        ld = u%10;
+        r = ld*(pow(10,j-i));
+        sum += r;
+        u /= 10;
         
-  
+    }
+    cout<<sum;
+
+    return 0;
 }
-    
-
-
 
 
     
